20233bc2c8940edcd57a83bc3aee04048dea3c5b
Yan Hulyi <yanguly@gmail.com>
2020-10-23 15:34:39 +0200

    create home page

---
+++ lib/home_page/page_button.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:la_isla_Bonita_ui/common_widgets/custom_raised_button.dart';
+
+class PageButton extends CustomRaisedButton {
+  PageButton({
+    String text,
+    Color color,
+    Color textColor,
+    VoidCallback onPressed,
+  }) : super(
+            child: Text(text,
+                style: TextStyle(
+                    color: textColor ?? Color(0xFFFBFCFB), fontSize: 15.0)),
+            color: color ?? Color(0xFF0E6555),
+            onPressed: onPressed);
+}
