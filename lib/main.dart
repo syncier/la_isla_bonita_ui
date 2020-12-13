3c0e965deb89a2b931e102cb34e24a9fae6f0e4f
Yan Hulyi <yanguly@gmail.com>
2020-12-13 15:18:02 +0100

    [IP-52]: add theming for app bar (#2)

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:la_isla_Bonita_ui/home_page/home_page.dart';
 import 'package:la_isla_Bonita_ui/sign_in/sign_in_page.dart';
+import 'package:la_isla_Bonita_ui/themes/theme.dart';
 
 void main() {
   runApp(MyApp());
@@ -11,9 +12,8 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'La Isla Bonita',
-      theme: ThemeData(
-          primarySwatch: Colors.green
-      ),
+      theme: AppTheme.lightTheme,
+      darkTheme: AppTheme.darkTheme,
       initialRoute: '/',
       routes: {
         '/': (context) => HomePage(),
