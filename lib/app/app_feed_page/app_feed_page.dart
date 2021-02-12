b75e8675a0665141f1d1d017ce606663b08c20ec
Yan Hulyi <yanguly@gmail.com>
2021-02-12 13:32:21 +0100

    [IP-26]: fix log in for IOS, run on IOS

--- lib/app/app_feed_page/app_feed_page.dart
+++ lib/app/app_feed_page/app_feed_page.dart
@@ -2,8 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:la_isla_Bonita_ui/app/act_page/act_page.dart';
 import 'package:la_isla_Bonita_ui/app/daily_page/daily_page.dart';
 import 'package:la_isla_Bonita_ui/app/profile_page/profile_page.dart';
-import 'package:la_isla_Bonita_ui/services/auth.dart';
-import 'package:provider/provider.dart';
 
 class AppFeedPage extends StatefulWidget {
   @override
@@ -30,7 +28,7 @@ class _AppFeedPageState extends State<AppFeedPage> {
     return Scaffold(
       body: Center(child: _navList.elementAt(_selectedIndex)),
       bottomNavigationBar: SizedBox(
-        height: 72.0,
+        height: 90.0,
         child: BottomNavigationBar(
           items: const <BottomNavigationBarItem>[
             BottomNavigationBarItem(
